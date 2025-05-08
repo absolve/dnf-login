50b94fba42cdb8b93f9031dd66cf7ca118843033
absolve <wangding1992@126.com>
2025-05-08 10:56:10 +0800

    更新功能

--- go-service/main.go
+++ go-service/main.go
@@ -2,17 +2,32 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	store := cookie.NewStore([]byte("secret"))
 	r := gin.Default()
+	r.Use(sessions.Sessions("mysession", store))
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
+	})
+
+	r.GET("/test", func(context *gin.Context) {
+		se := sessions.Default(context)
+		//var op sessions.Options
+		//op.MaxAge=100
+		//se.Options(op)
+		if se.Get("key") == nil {
+			se.Set("key", time.Now().GoString())
+		}
 	})
 	err := r.Run()
 	if err != nil {
